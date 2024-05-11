a02edb6ccb2bd831eb448920bc1181f39d25f5a1
Minjae Joe(Cho) <ussr1285@ajou.ac.kr>
2024-05-11 16:50:40 +0900

    [fix] 기존 SubletPostStore를 활용한 검색을 하기 위해서, 로컬 저장소(세션)에 데이터 저장되도록 수정.

--- src/@store/SubletPostStore.ts
+++ src/@store/SubletPostStore.ts
@@ -1,6 +1,6 @@
 import { Post } from "@type/Type";
 import { create } from "zustand";
-
+import { persist, createJSONStorage } from "zustand/middleware";
 const URL = process.env.NEXT_PUBLIC_BACKEND_URL + `/post?maxPost=6&page=`;
 
 export const SubletPostStore = create<{
@@ -15,36 +15,56 @@ export const SubletPostStore = create<{
   asyncGetPost: (page: number) => Promise<void>;
   asyncGetPostAll: () => Promise<void>;
   setPosts: (posts: Post[]) => void;
-}>()((set) => ({
-  post: [],
-  postExist: false,
-  page: 1,
-  postAll: [],
-  postMarker: false,
-  setPostMarker: (flag) => set(() => ({ postMarker: flag })),
-  // setPost: (post) => set(() => ({ post: [...post], postExist: true })),
-  clearPost: () => set(() => ({ post: [], postExist: false })),
-  clearPostAll: () => set(() => ({ postAll: [], postExist: false })),
-  asyncGetPost: async (page) => {
-    const json = await fetch(URL + page).then((response) => response.json());
-    set(() => ({ post: [...json], postExist: true }));
-  },
-  asyncGetPostAll: async () => {
-    let nowPage = 1;
-    let tempPostAll: Post[] = [];
-    while (true) {
-      const json = await fetch(URL + nowPage).then((response) =>
-        response.json()
-      );
+}>()(
+  persist(
+    (set) => ({
+      post: [],
+      postExist: false,
+      page: 1,
+      postAll: [],
+      postMarker: false,
+      setPostMarker: (flag) => set(() => ({ postMarker: flag })),
+      // setPost: (post) => set(() => ({ post: [...post], postExist: true })),
+      clearPost: () => set(() => ({ post: [], postExist: false })),
+      clearPostAll: () => set(() => ({ postAll: [], postExist: false })),
+      asyncGetPost: async (page) => {
+        const json = await fetch(URL + page).then((response) =>
+          response.json()
+        );
+        set(() => ({ post: [...json], postExist: true }));
+      },
+      asyncGetPostAll: async () => {
+        let nowPage = 1;
+        let tempPostAll: Post[] = [];
+        while (true) {
+          const json = await fetch(URL + nowPage).then((response) =>
+            response.json()
+          );
 
-      if (json.length === 0) {
-        set((state) => ({ postAll: [...tempPostAll], postExist: true }));
-        break;
-      }
-      tempPostAll = [...tempPostAll, ...json];
-      nowPage++;
+          if (json.length === 0) {
+            set((state) => ({ postAll: [...tempPostAll], postExist: true }));
+            break;
+          }
+          tempPostAll = [...tempPostAll, ...json];
+          nowPage++;
+        }
+      },
+      setPosts: (posts: Post[]) => {
+        set({
+          post: [...posts],
+          postExist: true,
+        });
+      },
+    }),
+    {
+      name: "subletPostStore-storage",
+      storage: createJSONStorage(() => sessionStorage),
     }
-  },
-  setPosts: (posts: Post[]) =>
-    set(() => ({ post: [...posts], postExist: true })),
-}));
+  )
+);
+
+// setSearchLocation: (city, gu) =>
+//   set({
+//     searchLocation: { city: city, gu: gu },
+//   }),
+// }),
