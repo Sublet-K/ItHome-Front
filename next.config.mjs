02109a8287dca2ede641a1606796b9d4781aea6a
cjfghk5697 <cjfghk5697@gmail.com>
2024-07-11 23:10:57 +0900

    add

--- next.config.mjs
+++ next.config.mjs
@@ -16,18 +16,7 @@ const nextConfig = {
     ],
   },
   env: {
-    PASSWORD:'1253ys5697@',
-    USERNAME:'evan',
-    NEXT_PUBLIC_BACKEND_URL:'https://localhost:4000',
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID:"206318961864-8s7ldu05gvpb28h3nepem9fdpfgut07n.apps.googleusercontent.com",
-    NEXT_PUBLIC_KAKAO_CLIENT_ID:"09bf01a9ff1495578ebe3b1dc2fb47b3",
-    NEXT_PUBLIC_NAVER_CLIENT_ID:"lGRf0bdS4VJox6O3SDdb",
-    NEXT_PUBLIC_KAKAO_REDIRECT_URI:'https://localhost:3000/oauth/kakao',
-    NEXT_PUBLIC_NAVER_REDIRECT_URI:'https://localhost:3000',
-    NEXT_PUBLIC_KAKAO_JS:"0a1100664012f98b0971df87496e5ca8",
-    NEXT_PUBLIC_KAKAO_INTEGRITY:'sha384-l+xbElFSnPZ2rOaPrU//2FF5B4LB8FiX5q4fXYTlfcG4PGpMkE1vcL7kNXI6Cci0',
-    NEXT_PUBLIC_TEST_BACKEND_URL:'https://localhost:4000',
-    FRONTEND_PORT:"3000",
+
   },
 };
 
