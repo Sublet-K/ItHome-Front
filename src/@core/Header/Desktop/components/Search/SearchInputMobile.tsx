53aa2f60341d2288aa5f0267b95b8c95b093037d
Minjae Joe <ipszone0525@gmail.com>
2024-08-02 22:24:46 +0900

    [Header] 모바일 검색 도구 토글 추가.

--- src/@core/Header/Desktop/components/Search/SearchInputMobile.tsx
+++ src/@core/Header/Desktop/components/Search/SearchInputMobile.tsx
@@ -6,29 +6,50 @@ import SearchLocation from "./components/SearchLocation";
 import SearchPriceRange from "./components/SearchPriceRange";
 
 const SearchInputMobile = ({ filterState, setFilterState }) => {
+  const [searchFilterShow, setSearchFilterShow] = useState(false);
+
+  const toggleSearchFilterShow = () => {
+    setSearchFilterShow(!searchFilterShow);
+  };
+
   return (
-    <div className="space-y-1 px-2 pb-3 pt-2 flex flex-col">
-      <MobileDashboardItem>
-        <SearchLocation
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
-      </MobileDashboardItem>
-      <MobileDashboardItem>
-        <SearchDate filterState={filterState} setFilterState={setFilterState} />
-      </MobileDashboardItem>
-      <MobileDashboardItem>
-        <SearchPriceRange
-          filterState={filterState}
-          setFilterState={setFilterState}
-        />
-      </MobileDashboardItem>
-      <div
-        className="sm:hidden border-t border-gray-300 flex items-center justify-center h-16"
-        id="mobile-menu"
-      >
-        <SearchButton setFilterState={setFilterState} />
+    <div className="pt-2">
+      <div className="flex justify-center">
+        <img src="/svgs/searchButton.svg" alt="search" />
+        {searchFilterShow ? (
+          <button onClick={toggleSearchFilterShow}>검색 닫기</button>
+        ) : (
+          <button onClick={toggleSearchFilterShow}>검색하기</button>
+        )}
       </div>
+      {searchFilterShow && (
+        <div className="space-y-1 px-2 pb-3 pt-2 flex flex-col">
+          <MobileDashboardItem>
+            <SearchLocation
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+          </MobileDashboardItem>
+          <MobileDashboardItem>
+            <SearchDate
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+          </MobileDashboardItem>
+          <MobileDashboardItem>
+            <SearchPriceRange
+              filterState={filterState}
+              setFilterState={setFilterState}
+            />
+          </MobileDashboardItem>
+          <div
+            className="sm:hidden border-t border-gray-300 flex items-center justify-center h-16"
+            id="mobile-menu"
+          >
+            <SearchButton setFilterState={setFilterState} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
