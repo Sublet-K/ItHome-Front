d9ba57e9bf911e78b67b25e18461cda45e8cbaac
Minjae Joe <ipszone0525@gmail.com>
2024-08-08 11:30:38 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   webpack: (config) => {
     // Unset client-side javascript that only works server-side
     config.resolve.fallback = { fs: false, module: false, path: false };
